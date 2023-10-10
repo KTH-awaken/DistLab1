9c0328ef44fd366b54230adaf0b8a54a63001290
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2023-10-10 17:26:34 +0200

    redovining

--- src/main/java/com/example/distlab1/Filters/UnauthenticatedFilter.java
+++ src/main/java/com/example/distlab1/Filters/UnauthenticatedFilter.java
@@ -1,28 +1,28 @@
-//package com.example.distlab1.Filters;
-//
-//import jakarta.servlet.*;
-//import jakarta.servlet.annotation.WebFilter;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpSession;
-//
-//import java.io.IOException;
-//
-//
-//@WebFilter(
-//        urlPatterns = {"/login", "/sign-up"},
-//        filterName = "login-sign-up-filter"
-//)
-//public class UnauthenticatedFilter implements Filter {
-//
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        HttpServletRequest req = (HttpServletRequest) request;
-//
-//        HttpSession session = req.getSession(false);
-//        if(session != null){
-//            if(session.getAttribute("user") == null){
-//                chain.doFilter(request,response);
-//            }
-//        }
-//
-//    }
-//}
+package com.example.distlab1.Filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+@WebFilter(
+        urlPatterns = {"/login", "/sign-up"},
+        filterName = "login-sign-up-filter"
+)
+public class UnauthenticatedFilter implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        HttpSession session = req.getSession(false);
+        if(session != null){
+            if(session.getAttribute("user") == null){
+                chain.doFilter(request,response);
+            }
+        }
+
+    }
+}
